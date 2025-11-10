daf7115201f62cee14964654ab492fac7b54826f
Nereka38 <neerefc@gmail.com>
2025-11-10 21:02:04 +0100

    feat: add wrangler configuration for Cloudflare Workers and ensure proper formatting in next.config.ts and package.json

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        optimizePackageImports: ["@chakra-ui/react"],
-      },
+  output: 'export', // habilita la exportación estática
+  images: {
+    unoptimized: true, // evita optimización de imágenes que requiere servidor
+  },
+  experimental: {
+    optimizePackageImports: ["@chakra-ui/react"],
+  },
 }
 
 module.exports = nextConfig
